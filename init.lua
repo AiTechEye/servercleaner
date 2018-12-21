2607fd52d30d5fed1ea55d7c875e8bb7c42234ab
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2018-12-21 22:22:58 +0100

    Add files via upload

--- init.lua
+++ init.lua
@@ -101,6 +101,8 @@ minetest.register_chatcommand("delplayer", {
 	func = function(name, param)
 		if param=="" then
 			return false,"/delplayer <playername>"
+		elseif name==param then
+			return false,"delete a player, not yourself, use '/delme' instead"
 		elseif minetest.check_player_privs(param, {scmoderator=true}) and not minetest.check_player_privs(name, {scadmin=true}) then
 			return false,"Moderators can't delete moderators"
 		end
@@ -146,7 +148,7 @@ minetest.register_chatcommand("delme", {
 					minetest.chat_send_player(name,"The time (20) has expired")
 				end
 			end,name)
-			return false,"Are you sure you want to delete your account?\nAll your privileges, protected/areas will be deleted, locked stuff will be able for other people.\nTo confirm type: /delme delete me"
+			return false,"Are you sure you want to delete your account?\nAll your privileges, protected/areas/data will be deleted, locked stuff will be able for other people.\nTo confirm type: /delme delete me"
 		elseif servercleaner.delme[name] and param=="delete me" then
 			servercleaner.delete_player(name,name)
 		end
